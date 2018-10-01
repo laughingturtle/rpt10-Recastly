c36677738dbe5ee9130d41df4ebfc00a8c72e504
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-10-01 15:29:40 -0700

    Complete rendering App component to the DOM

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,11 +1,30 @@
-var VideoListEntry = () => (
+// class VideoListEntry extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div className="video-list-entry media">
+//         <div className="media-left media-middle">
+//           <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+//         </div>
+//         <div className="media-body">
+//           <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
+//           <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
       <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
 );
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,31 @@
-var VideoPlayer = () => (
+// class VideoPlayer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div className="video-player">
+//         <div className="embed-responsive embed-responsive-16by9">
+//           <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+//         </div>
+//         <div className="video-player-details">
+//           <h3>{this.props.video.snippet.title}</h3>
+//           <div>{this.props.video.snippet.description}</div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
